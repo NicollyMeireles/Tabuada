bbcb48db6a42f02c3e1faa2dd91eaaaf3fb2467a
Nicolly Meireles <Nicollymeireles32@gmail.com>
2023-06-02 21:37:41 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("  Tabuada ");
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("Digite um número: ");
+
+Console.ForegroundColor = ConsoleColor.DarkBlue; 
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+int multiplo = 1;
+int resultado;
+
+
+while ( multiplo <= 10)
+{
+    resultado = numeroDigitado * multiplo;
+    Console.WriteLine($"{numeroDigitado} X {multiplo} = {resultado}");
+    multiplo++;
+}
